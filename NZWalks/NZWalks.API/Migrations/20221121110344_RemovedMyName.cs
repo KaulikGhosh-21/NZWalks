5f175ea79727ad9ebe14abd6928bee9667061ca7
Kaulik Ghosh <kaulik01ghosh@gmail.com>
2022-11-21 16:43:11 +0530

    Migartion done.

---
+++ NZWalks/NZWalks.API/Migrations/20221121110344_RemovedMyName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedMyName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyName",
+                table: "WalkDifficulty");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MyName",
+                table: "WalkDifficulty",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
